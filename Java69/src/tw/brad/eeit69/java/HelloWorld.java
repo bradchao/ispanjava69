1f67da49238c6c5497f0ff368776bd86b0de1594
brad <brad@braddembp>
2023-06-12 11:54:48 +0800

    44

--- Java69/src/tw/brad/eeit69/java/HelloWorld.java
+++ Java69/src/tw/brad/eeit69/java/HelloWorld.java
@@ -3,7 +3,7 @@ package tw.brad.eeit69.java;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello, WorldV3");
+		System.out.println("Hello, WorldV4");
 	}
 
 }
